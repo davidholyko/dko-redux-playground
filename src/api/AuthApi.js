4e1a47c7076729b7b8598e4ec88e5c5e9ff79ef4
David Ko <david.holy.ko@gmail.com>
2019-06-16 11:58:25 -0400

    chore(linting): lint entire project

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -1,5 +1,5 @@
-import apiUrl from './apiConfig'
-import axios from 'axios'
+import apiUrl from './apiConfig';
+import axios from 'axios';
 
 export const signUp = credentials => {
   return axios({
@@ -12,8 +12,8 @@ export const signUp = credentials => {
         password_confirmation: credentials.passwordConfirmation
       }
     }
-  })
-}
+  });
+};
 
 export const signIn = credentials => {
   return axios({
@@ -25,25 +25,25 @@ export const signIn = credentials => {
         password: credentials.password
       }
     }
-  })
-}
+  });
+};
 
 export const signOut = user => {
   return axios({
     url: apiUrl + '/sign-out',
     method: 'DELETE',
     headers: {
-      'Authorization': `Token token=${user.token}`
+      Authorization: `Token token=${user.token}`
     }
-  })
-}
+  });
+};
 
 export const changePassword = (passwords, user) => {
   return axios({
     url: apiUrl + '/change-password',
     method: 'PATCH',
     headers: {
-      'Authorization': `Token token=${user.token}`
+      Authorization: `Token token=${user.token}`
     },
     data: {
       passwords: {
@@ -51,5 +51,5 @@ export const changePassword = (passwords, user) => {
         new: passwords.newPassword
       }
     }
-  })
-}
+  });
+};
